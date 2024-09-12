6f5d4a138ba6b2538f81bb9d4f574b2650515271
mbrdevteam <pm2@mbrinformatics.com>
2024-09-12 15:03:16 +0530

    apps updated

---
+++ frappe-bench/apps/hrms/hrms/public/frontend/assets/realtime-S0CN1eiH.js
@@ -0,0 +1,2 @@
+import{Q as u}from"./frappe-ui-CNy1brZ0.js";const r=u({});function c(e,i,t){n(e,i),e.on("list_update",s=>{s.doctype==i&&t(s.name)})}function n(e,i){r[i]||(e.emit("doctype_subscribe",i),r[i]=!0)}export{c as u};
+//# sourceMappingURL=realtime-S0CN1eiH.js.map
