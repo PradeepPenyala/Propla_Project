030808ca62f2669693848ef6d9a42d195e57dbbc
mbrdevteam <pm2@mbrinformatics.com>
2025-02-26 18:35:29 +0530

    Updated code

--- frappe-bench/apps/erpnext/erpnext/hooks.py
+++ frappe-bench/apps/erpnext/erpnext/hooks.py
@@ -7,7 +7,7 @@ app_color = "#e74c3c"
 app_email = "info@erpnext.com"
 app_license = "GNU General Public License (v3)"
 source_link = "https://github.com/frappe/erpnext"
-app_logo_url = "/assets/erpnext/images/erpnext-logo.svg"
+app_logo_url = "/assets/erpnext/images/image.png"
 
 
 develop_version = "14.x.x-develop"
@@ -92,8 +92,8 @@ calendars = ["Task", "Work Order", "Sales Order", "Holiday List", "ToDo"]
 website_generators = ["BOM", "Sales Partner"]
 
 website_context = {
-	"favicon": "/assets/erpnext/images/erpnext-favicon.svg",
-	"splash_image": "/assets/erpnext/images/erpnext-logo.svg",
+	"favicon": "/assets/erpnext/images/favcon.png",
+	"splash_image": "/assets/erpnext/images/image.png",
 }
 
 # nosemgrep
@@ -460,7 +460,7 @@ scheduler_events = {
 	],
 }
 
-email_brand_image = "assets/erpnext/images/erpnext-logo.jpg"
+email_brand_image = "assets/erpnext/images/propla_logo1.png"
 
 default_mail_footer = """
 	<span>
--- frappe-bench/apps/frappe/frappe/core/doctype/user/user.py
+++ frappe-bench/apps/frappe/frappe/core/doctype/user/user.py
@@ -60,7 +60,6 @@ class User(Document):
 		birth_date: DF.Date | None
 		block_modules: DF.Table[BlockModule]
 		bypass_restrict_ip_check_if_2fa_enabled: DF.Check
-		default_app: DF.Literal[None]
 		default_workspace: DF.Link | None
 		defaults: DF.Table[DefaultValue]
 		desk_theme: DF.Literal["Light", "Dark", "Automatic"]
@@ -1368,3 +1367,5 @@ def impersonate(user: str, reason: str):
 	notification.set("type", "Alert")
 	notification.insert(ignore_permissions=True)
 	frappe.local.login_manager.impersonate(user)
+
+
